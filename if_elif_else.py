4b6c2f3ee3ff38073d5977167a96fe8f0fe43524
shrikant jagtap <shrikantjagtap@gmail.com>
2016-12-25 19:11:14 +0530

    if else in python

---
+++ if_elif_else.py
@@ -0,0 +1,10 @@
+## if else part in python
+
+if 10 in range(11):
+	print "value 10 is available in list"
+elif 11 in range(11):
+	print "value 11 is available in list"
+else:
+	print "value not found in list"
+
+
