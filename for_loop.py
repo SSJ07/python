6e315fc8ec09fb9c7b55eaa65836125ad0518bb1
shrikant jagtap <shrikantjagtap@gmail.com>
2016-12-25 19:10:55 +0530

    for loop in python

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+## for loop in python
+
+for i in range(10):
+	print i, " ", 
+
+print "\nprint value 10 to 100 increment by 5"
+
+for i in range(10, 100, 5):
+	print i, " ", 
+
+
+print "\nelse part in for loop:"
+
+for i in range(10):
+	print i, " ",
+else:
+	print "else part in for loop"
+
