8351fcc993e7869f712014bf60c16646d5bc256a
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-11 17:50:24 +0530

    abs function example

---
+++ built_in_functions/abs.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 11, 2018
+
+@author: shrikant_jagtap
+'''
+
+'''
+    abs function return absolute value of given number
+'''
+
+
+print("absolute value of -1 : {}".format(abs(-1)))
+print("absolute value of -1 : {}".format(abs(-10.99)))
