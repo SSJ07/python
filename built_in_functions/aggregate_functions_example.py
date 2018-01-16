1096686264049d2af724705dcaeaa6bc568f1eca
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-17 00:43:38 +0530

    python aggregate functions example

---
+++ built_in_functions/aggregate_functions_example.py
@@ -0,0 +1,8 @@
+'''
+    Aggregate function like sum(), max(), min()
+'''
+
+print("sum of 1 to 10 is {}".format(sum(range(11))))
+print("max in 1 to 100 is {}".format(max(range(1,101))))
+print("min in 1 to 100 is {}".format(min(range(1,101))))
+
