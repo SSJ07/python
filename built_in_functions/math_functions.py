fb645976477232af59b2d819ebeb042e71c65e28
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-18 13:07:44 +0530

    math module functions in python

---
+++ built_in_functions/math_functions.py
@@ -0,0 +1,33 @@
+'''
+Created on Jan 18, 2018
+
+@author: shrikant_jagtap
+'''
+from math import sqrt, log2, log10, exp, floor, factorial, fmod, trunc
+
+'''
+    Multiple common function of math module are:
+    
+    1. sqrt
+    2. pow(x, y)
+    3. log2
+    4. exp
+    5. floor(x)
+    6. fmod(x, y)
+    7. trunc(x)
+    8. factorial(x)
+    9. ceil(x)
+'''
+
+print("square root of 225 is : {}".format(sqrt(225)))
+print("power of 5 of 3 is :{}".format(pow(5, 3)))
+print("log2 of 15 is : {}".format(log2(15)))
+print("log10 of 111 is :{}".format(log10(111)))
+print("exp example is :{}".format(exp(5**3)))
+print("floor function example is : {}".format(floor(15.9)))
+print("floor function example is : {}".format(floor(15.9)))
+print("factorial of 5 is :{}".format(factorial(5)))
+print("5 mod 3 is : {}".format(fmod(5,3)))
+print("truncated value of 15.753453 is : {}".format(trunc(15.753453)))
+print("ceil(15) returns :{}".format(15))
+
