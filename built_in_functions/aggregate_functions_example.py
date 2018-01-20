218a9f90d8e8dcf7f5c3162b0f90f85eb0b0ab92
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-21 00:05:11 +0530

    Update aggregate_functions_example.py

--- built_in_functions/aggregate_functions_example.py
+++ built_in_functions/aggregate_functions_example.py
@@ -1,6 +1,13 @@
 '''
     Aggregate function like sum(), max(), min()
 '''
+'''
+Sum() function returns sum of given integer list.
+max(): returns max value of given integer list.
+min(): refurns min value of given integer list.
+'''
+
+
 
 print("sum of 1 to 10 is {}".format(sum(range(11))))
 print("max in 1 to 100 is {}".format(max(range(1,101))))
