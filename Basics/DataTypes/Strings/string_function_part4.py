812ec02866196972232ab6a000f595df744f08c8
Shrikant Jagtap <jagtap.shrikant91@gmail.com>
2022-08-06 14:17:48 +0530

    string basic

--- Basics/DataTypes/Strings/string_function_part4.py
+++ Basics/DataTypes/Strings/string_function_part4.py
@@ -27,3 +27,78 @@ This module covers below string functions:
 	  Adjust string right justified with given char.
 
 """
+# pylint: disable=invalid-name
+
+example = "Code24"
+print("isalnum(): ", example.isalnum())
+
+example = "Code24 "
+print("isalnum(): ", example.isalnum())
+
+example = "Code24i\n"
+print("isalnum(): ", example.isalnum())
+
+example = "Code"
+print("isalnum(): ", example.isalnum())
+print("isalpha(): ", example.isalpha())
+
+example = "100"
+print("isalnum(): ", example.isalnum())
+print("isalpha(): ", example.isalpha())
+
+# isdecimal
+print("isdecimal(): ", example.isdecimal())
+print("isdecimal(): ", "A".isdecimal())
+print("isdecimal(): ", "B".isdecimal())
+print("isdecimal(): ", "b101".isdecimal())
+
+#  isdigit
+print("isdigit(): ", "101".isdigit())
+print("isdigit(): ", "A123213".isdigit())
+print("isdigit(): ", "101".isdigit())
+print("isdigit(): ", "101 ".isdigit())
+
+# isidentifier
+message = "$message"
+print("isidentifier: ", message.isidentifier())
+
+message = "_message"
+print("isidentifier: ", message.isidentifier())
+
+message = "10message"
+print("isidentifier: ", message.isidentifier())
+
+#  isnumeric
+print("isnumeric(): ", "101".isnumeric())
+print("isnumeric(): ", "A123213".isnumeric())
+print("isnumeric(): ", "10123131".isnumeric())
+
+# isprintable
+print("\t".isprintable())
+print("\n".isprintable())
+print("message".isprintable())
+print("100".isprintable())
+
+# isspace
+print("-"*10, "str.isspace", "-"*10)
+print(" ".isspace())
+print("                             ".isspace())
+print(" hello".isspace())
+print(" hello ".isspace())
+print(" hello python ".isspace())
+
+print("-"*10, "str.istitle", "-"*10)
+print("hello".istitle())
+print("Hello python".istitle())
+print("Hello Python".istitle())
+print("".istitle())
+
+print("-"*10, "str.ljust", "-"*10)
+print("hello".ljust(50))
+print("hello".ljust(70, "-"))
+print("hello".ljust(100, "*"))
+
+print("-"*10, "str.rjust", "-"*10)
+print("hello".rjust(50))
+print("hello".rjust(70, "-"))
+print("hello".rjust(100, "*"))
